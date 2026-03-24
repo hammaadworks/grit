8c9510b7293a937cff04e042ffc81a6e926bbc20
hammaadworks <hammaadworks@gmail.com>
2026-03-24 20:26:50 +0530

    added website

---
+++ website/next.config.mjs
@@ -0,0 +1,14 @@
+import createMDX from '@next/mdx';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  images: {
+    unoptimized: true,
+  },
+};
+
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
